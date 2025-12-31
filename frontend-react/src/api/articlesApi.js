b11df376ca61806d1cc37610b32d25024235ebe3
MysticalFlame3 <suryaraj.9274@gmail.com>
2025-12-31 17:11:09 +0530

    minor update

--- frontend-react/src/api/articlesApi.js
+++ frontend-react/src/api/articlesApi.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://127.0.0.1:8000/api';
+const API_BASE_URL = 'import.meta.env.VITE_API_URL';
+
+if (!API_BASE_URL) {
+  throw new Error('VITE_API_URL is not defined');
+}
 
 export const fetchArticles = async () => {
     try {
