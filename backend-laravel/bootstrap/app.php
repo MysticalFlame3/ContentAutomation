f035ddc8d82b6bf519f77fd536047c8b6753d973
MysticalFlame3 <suryaraj.9274@gmail.com>
2025-12-31 15:50:59 +0530

    fixed cors

--- backend-laravel/bootstrap/app.php
+++ backend-laravel/bootstrap/app.php
@@ -11,9 +11,27 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
+    ->withMiddleware(function (Middleware $middleware) {
+
+        $middleware->append(function ($request, $next) {
+
+            if ($request->getMethod() === 'OPTIONS') {
+                return response('', 204)
+                    ->header('Access-Control-Allow-Origin', '*')
+                    ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+                    ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+            }
+
+            $response = $next($request);
+
+            $response->headers->set('Access-Control-Allow-Origin', '*');
+            $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+            $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+            return $response;
+        });
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create();
+    })
+    ->create();
